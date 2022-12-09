0d212593cda90a16048c05526ca33606130281c6
HrithikDhongadi <hrithikdhongadi@gmail.com>
2022-12-09 18:50:08 +0530

    Speed Update

--- amr_ws/src/motor_controller/src/joyStickVelPubV2.py
+++ amr_ws/src/motor_controller/src/joyStickVelPubV2.py
@@ -14,7 +14,7 @@ RIGHT_STICK_LR = 3
 RIGHT_STICK_UP = 4
 DEADZONE = 0.11
 
-MAX_LINEAR_VEL = 0.1
+MAX_LINEAR_VEL = 0.15
 MAX_ANGULAR_VEL = 0.5
 
 
@@ -56,7 +56,7 @@ if __name__ == "__main__":
         # if(msg.angular.z < 0.05 or msg.angular.z > -0.05):
         #     msg.angular.z = 0
 
-        msg.linear.x = (MAX_LINEAR_VEL/2)*(xbox.get_axis(RIGHT_TRIGGER)+1) - 0.15*(xbox.get_axis(LEFT_TRIGGER)+1)
+        msg.linear.x = (MAX_LINEAR_VEL/2)*(xbox.get_axis(RIGHT_TRIGGER)+1) - (MAX_LINEAR_VEL/2)*(xbox.get_axis(LEFT_TRIGGER)+1)
         
         if(xbox.get_axis(LEFT_STICK_LR) > DEADZONE or xbox.get_axis(LEFT_STICK_LR) < -DEADZONE):
             msg.angular.z = -(xbox.get_axis(LEFT_STICK_LR)) * MAX_ANGULAR_VEL
