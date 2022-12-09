ce15ddfdafbd10afe69157c995e3de77acc30bd8
Hrithik Dhongadi <86490172+HrithikDhongadi@users.noreply.github.com>
2022-12-09 14:36:04 +0530

    Delete amr_ws/build directory

--- amr_ws/build/motor_controller/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/hrithik/noetic/amr_ws/src/motor_controller
-# Build directory: /home/hrithik/noetic/amr_ws/build/motor_controller
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
